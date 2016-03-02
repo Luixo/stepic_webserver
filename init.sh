622e0747470201b2d1ed16fec5316d15ebf8704c
luixo <lexx92@mail.ru>
2016-03-03 02:11:28 +0300

    Sample files in directories.

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
+git config --global push.default matching
+git config --global user.name luixo
+git config --global user.email lexx92@mail.ru
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
