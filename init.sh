177279fe02d81f01eeb6c3d5fb6309d3ff52212b
luixo <lexx92@mail.ru>
2016-03-10 00:04:05 +0300

    Some refactoring

--- init.sh
+++ init.sh
@@ -5,4 +5,7 @@ git config --global user.email lexx92@mail.ru
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/django.wsgi /etc/gunicorn.d/django.wsgi
-sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/gunicorn restart
+
+kill -HUP /home/box/web/etc/gunicorn.pid
+gunicorn -c gunicorn.conf.py /etc/gunicorn.d/django.wsgi
