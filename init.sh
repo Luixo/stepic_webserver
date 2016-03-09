0a70063340d2748340184f9247f563fa7c0e41e6
a.immoreev <a.immoreev@qiwi.ru>
2016-03-09 19:35:15 +0300

    Added gunicorn configuration

--- init.sh
+++ init.sh
@@ -4,3 +4,5 @@ git config --global user.email lexx92@mail.ru
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
