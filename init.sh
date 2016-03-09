a61418727142e90ecd89806adf8bbba79db16711
a.immoreev <a.immoreev@qiwi.ru>
2016-03-09 19:48:44 +0300

    Fix IP and init

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ git config --global user.email lexx92@mail.ru
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/test.gunicorn /etc/gunicorn.d/test.swgi
+sudo ln -sf /home/box/web/etc/test.gunicorn /etc/gunicorn.d/test.wsgi
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf.py
 sudo /etc/init.d/gunicorn restart
